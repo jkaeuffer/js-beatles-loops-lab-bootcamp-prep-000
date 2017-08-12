0a2f2da3f2083db1bdfec7cfdea9ab25dccb3a90
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-12 02:07:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musician, instrument) {
   var theBeatles = [];
   var string = " plays "
-  for (let i = 0; i < musician.length 1; i++) {
+  for (let i = 0; i < musician.length; i++) {
     theBeatles.push(musician[i] + string + instrument[i])
   }
   return theBeatles
