eef59e03cf618d6b378460a69fb32a10e292f524
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-12 02:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function theBeatlesPlay(musician, instrument) {
   var theBeatles = new array();
-  
+  for (let i = 0; i < musician.length - 1, i++) {
+    theBeatles.push(`$musician[i] plays $instrument[i]`)
+  }
+  return theBeatles
+
 };
 
 function johnLennonFacts(array) {
