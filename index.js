3701c7ea76d7e71a86ab346c28bfd862f269e3e6
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-12 02:03:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musician, instrument) {
-  var theBeatles = new array();
+  var theBeatles = [];
   for (let i = 0; i < musician.length - 1; i++) {
     theBeatles.push(`$musician[i] plays $instrument[i]`)
   }
